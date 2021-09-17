f0f2500606064c6a9b99c6ec6c26c021084577f2
Chapa <chapaog@gmail.com>
2021-09-17 17:32:53 +0200

    Añadida teoria innerHTML y ejercicio innerHTML hecho.

--- Ejercicios/3cambioContenidoHtml.js
+++ Ejercicios/3cambioContenidoHtml.js
@@ -1,6 +1,10 @@
-var a = parseInt(document.getElementById("numero"));
-if (a % 2 == 0) {
-	document.getElementById("idDiv").innerHTML = ("El número " + a + " es múltiplo de 2.");
-} else {
-	document.getElementById("idDiv").innerHTML = ("El número " + a + " NO múltiplo de 2.");
+
+
+function esMultiplo(){
+	var a = parseInt(document.getElementById("numero").value);
+	if (a % 2 == 0) {
+		document.getElementById("respuesta").innerHTML = "El número " + a + " es múltiplo de 2.";
+	} else {
+		document.getElementById("respuesta").innerHTML = "El número " + a + " NO es múltiplo de 2.";
+	}
 }
